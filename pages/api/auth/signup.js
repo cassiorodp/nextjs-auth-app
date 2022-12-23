06a0dad17a140993297e17a533d5c997b72614e7
Cássio Pereira <cassiorodp@gmail.com>
2022-12-23 15:52:24 -0300

    fix: add already registered email

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,34 +2,45 @@ import { hashPassword } from '../../../lib/auth';
 import { connectToDatabase } from '../../../lib/db';
 
 async function handler(req, res) {
-  const data = req.body;
+  if (req.method === 'POST') {
+    const data = req.body;
 
-  const { email, password } = data;
+    const { email, password } = data;
 
-  if (
-    !email ||
-    !email.includes('@') ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({ message: 'Invalid input.' });
-    return;
-  }
+    if (
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
 
-  try {
-    const client = await connectToDatabase();
+    try {
+      const client = await connectToDatabase();
 
-    const db = client.db();
+      const db = client.db();
 
-    const hashedPassword = await hashPassword(password);
+      const existingUser = await db.collection('users').findOne({ email });
 
-    const result = await db
-      .collection('users')
-      .insertOne({ email, password: hashedPassword });
+      if (existingUser) {
+        res.status(422).json({ message: 'User exists already!' });
+        client.close();
+        return;
+      }
 
-    res.status(201).json({ message: 'Created user!' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error creating user.' });
+      const hashedPassword = await hashPassword(password);
+
+      const result = await db
+        .collection('users')
+        .insertOne({ email, password: hashedPassword });
+
+      res.status(201).json({ message: 'Created user!' });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Error creating user.' });
+    }
   }
 }
 
