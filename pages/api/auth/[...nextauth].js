846bf0573af14e735e3d4e94e7c0da8245ef56cc
Cássio Pereira <cassiorodp@gmail.com>
2022-12-26 13:48:52 -0300

    feat: add auth route logic

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,40 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { verifyPassword } from '../../../lib/auth';
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        if (!email || !password) {
+          throw new Error('Please enter an email and password');
+        }
+
+        const client = await connectToDatabase();
+
+        const usersColleciton = client.db().collection('users');
+
+        const user = await usersColleciton.findOne({ email });
+
+        if (!user) {
+          throw new Error('No user found!');
+        }
+
+        const isValid = await verifyPassword(password, user.password);
+
+        if (!isValid) {
+          throw new Error('Could not log you in!');
+        }
+
+        client.close();
+
+        return { email: user.email };
+      },
+    }),
+  ],
+});
