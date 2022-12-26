f736b5333e2a7f06274aa86345a3148fe03af19b
Cássio Pereira <cassiorodp@gmail.com>
2022-12-26 15:36:37 -0300

    fix: connectToDatabase

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 import { verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 
 export default NextAuth({
   session: {
