c85a7b1abc527066f8f610eb2e2d4847b62dd32a
Cássio Pereira <cassiorodp@gmail.com>
2022-12-26 17:34:54 -0300

    feat: add change password logic

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import classes from './profile-form.module.css';
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const [newPassword, setNewPassword] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    props.onChangePassword({
+      oldPassword,
+      newPassword,
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+          type="password"
+          id="new-password"
+        />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input
+          value={oldPassword}
+          onChange={(e) => setOldPassword(e.target.value)}
+          type="password"
+          id="old-password"
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,11 +1,25 @@
+import axios from 'axios';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
+  const changePasswordHandler = async (passwordData) => {
+    try {
+      const response = await axios.patch(
+        '/api/user/change-password',
+        passwordData
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
